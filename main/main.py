d893c5b2d4a321e9caa3443e8889f04cdb903193
Oleksii Petrushynskyi <petrushynskyi.oleksii@gmail.com>
2018-06-25 15:59:12 +0300

    Update bot 2.0
      1. Update cinema, days keyboards.
      2. Create parsers for `planetakino.ua`.
        2.1. Description parser.
        2.2. Sessions parser.
        2.3. Planetakino XML data parser.

--- main/main.py
+++ main/main.py
@@ -1,37 +1,35 @@
-"""Simple telegram bot."""
+"""Telegram bot."""
+
+from datetime import datetime, timedelta
 
 from telebot import TeleBot, types
-from config import token
+
+from config import token, cinema_dict
+from parser import get_films_data
 
 bot = TeleBot(token)
-
 film_dict = {}
 
-cinema_dict = {'Forum': 'https://planetakino.ua/lvov2/showtimes/',
-               'Kingcross': 'https://planetakino.ua/lvov/showtimes/'}
-period_dict = {'Today': 'one-day',
-               'Tomorrow': 'tomorrow',
-               'Week': 'week',
-               'Month': 'month'}
 
-
-class TripToCinema:
-    """Class that represent data about trip to the cinema."""
+class FilmSession:
+    """Class that represent film session data."""
 
     def __init__(self):
         """Initialize data."""
+        self.today = datetime.today()
         self.cinema = None
         self.period = None
-        self.from_time = 0
-        self.to_time = 0
 
 
 @bot.message_handler(commands=['help', 'start'])
 def handle_start_command(message):
-    """Show keyboard."""
-    markup_main = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=False)
-    markup_main.row('Хочу сходити на фільмєц.')
-    bot.send_message(message.from_user.id, 'Обери одну з кнопок нижче наведених.', reply_markup=markup_main)
+    """Show keyboard for cinema choise."""
+    markup_cinemas = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+    markup_cinemas.row('Forum', 'Kingcross')
+    msg = bot.send_message(message.from_user.id,
+                           'Хочеш подивитись фільмєц? Обери кінотеатр:  \U00002935',
+                           reply_markup=markup_cinemas)
+    bot.register_next_step_handler(msg, process_cinema_step)
 
 
 @bot.message_handler(commands=['stop'])
@@ -41,50 +39,65 @@ def handle_stop_command(message):
     bot.send_message(message.from_user.id, 'Покєда.', reply_markup=hide_markup)
 
 
-@bot.message_handler(content_types=['text'])
-def handle_buttons(message):
-    """Perform the action according to the request."""
-    if message.text == 'Хочу сходити на фільмєц.':
-        markup_cinemas = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
-        markup_cinemas.row('Forum', 'Kingcross')
-        msg = bot.send_message(message.from_user.id, 'Куди?', reply_markup=markup_cinemas)
-        bot.register_next_step_handler(msg, process_cinemas_step)
+def process_cinema_step(message):
+    """Create film object, update cinema field of created object."""
+    film = FilmSession()
+    days = []
+    for i in range(1, 4):
+        day = film.today + timedelta(days=1 + i)
+        days.append(day.strftime('%d.%m, %a'))
 
-
-def process_cinemas_step(message):
-    """Create film object and update cinema field of film object with given data."""
     try:
         chat_id = message.chat.id
-        film = TripToCinema()
         film_dict[chat_id] = film
         film.cinema = cinema_dict[message.text]
 
-        markup_period = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
-        markup_period.row('Today', 'Tomorrow', 'Week', 'Month')
-        msg = bot.send_message(message.from_user.id, 'Період?', reply_markup=markup_period)
-        bot.register_next_step_handler(msg, process_time_step)
+        markup_days = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+        markup_days.row('Сьогодні', 'Завтра')
+        markup_days.row(days[0], days[1], days[2])
+        msg = bot.send_message(message.from_user.id,
+                               'А тепер день:  \U00002935',
+                               reply_markup=markup_days)
+        bot.register_next_step_handler(msg, process_days_step)
 
     except Exception as e:
         print(e)
-        bot.reply_to(message, 'Упс..')
+        bot.reply_to(message, 'Упс..Щось пішло не так. Cпробуй ще раз (/start)')
 
 
-def process_time_step(message):
+def process_days_step(message):
     """Update period field of film object with given data."""
     try:
         chat_id = message.chat.id
         film = film_dict[chat_id]
-        film.period = period_dict[message.text]
-        data = get_films_data(film) # noqa
+
+        if message.text == 'Сьогодні':
+            film.period = film.today
+        elif message.text == 'Завтра':
+            film.period = film.today + timedelta(days=1)
+        else:
+            import re
+            date_information = re.split('[.,]', message.text)
+            film.period = datetime(film.today.year,
+                                   int(date_information[1]),
+                                   int(date_information[0]))
+
+        bot.send_message(message.from_user.id, 'Це займе декілька секунд...')
+        data = get_films_data(film)
+        for key, value in data.items():
+            title = key
+            url = value.get('url')
+            sessions = ', '.join(value.get('sessions'))
+            description = value.get('description')
+
+            formed_msg = '\U0001F538{}.\n{}\n' \
+                         '\U0001F538Доступні сесії: {}.\n{}'.format(title, description, sessions, url)
+
+            bot.send_message(message.from_user.id, formed_msg)
+
     except Exception as e:
         print(e)
-        bot.reply_to(message, 'oooops')
-
-
-def get_films_data(film):
-    """Get data about films from `planatekino`."""
-    # TODO
-    pass
+        bot.reply_to(message, 'Упс..Щось пішло не так. Cпробуй ще раз (/start).')
 
 
 if __name__ == '__main__':
